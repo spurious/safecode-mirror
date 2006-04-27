a3699aef4d99869a60f11c4d26f83ee1478bc87e
dhurjati <dhurjati@91177308-0d34-0410-b5e6-96231b3b80d8>
2006-04-27 20:50:44 +0000

    checking in my changes for SAFECode final paper
    git-svn-id: http://llvm.org/svn/llvm-project/safecode/trunk@87211 91177308-0d34-0410-b5e6-96231b3b80d8

--- include/InsertPoolChecks.h
+++ include/InsertPoolChecks.h
@@ -4,7 +4,7 @@
 #include "llvm/Pass.h"
 #include "ConvertUnsafeAllocas.h"
 
-#define LLVA_KERNEL
+//#define LLVA_KERNEL
 
 #ifndef LLVA_KERNEL
 #include "SafeDynMemAlloc.h"
@@ -23,34 +23,42 @@ struct InsertPoolChecks : public ModulePass {
     virtual void getAnalysisUsage(AnalysisUsage &AU) const {
       AU.addRequired<ConvertUnsafeAllocas>();
 //      AU.addRequired<CompleteBUDataStructures>();
-      AU.addRequired<TDDataStructures>();
+//      AU.addRequired<TDDataStructures>();
 #ifndef LLVA_KERNEL      
       AU.addRequired<EquivClassGraphs>();
       AU.addRequired<PoolAllocate>();
       AU.addRequired<EmbeCFreeRemoval>();
+      AU.addRequired<TargetData>();
+#else 
+      AU.addRequired<TDDataStructures>();
 #endif
+      
     };
     private :
       CUA::ConvertUnsafeAllocas * cuaPass;
 #ifndef  LLVA_KERNEL
-      PoolAllocate * paPass;
+  PoolAllocate * paPass;
   EquivClassGraphs *equivPass;
   EmbeCFreeRemoval *efPass;
+  TargetData * TD;
 #else
   TDDataStructures * TDPass;
 #endif  
   Function *PoolCheck;
   Function *ExactCheck;
+  Function *FunctionCheck;
   void addPoolCheckProto(Module &M);
   void addPoolChecks(Module &M);
   void addGetElementPtrChecks(Module &M);
   DSNode* getDSNode(const Value *V, Function *F);
   unsigned getDSNodeOffset(const Value *V, Function *F);
+  void addLoadStoreChecks(Module &M);
 #ifndef LLVA_KERNEL  
-  Value * getPoolHandle(const Value *V, Function *F, PA::FuncInfo &FI);
+  void addLSChecks(Value *Vnew, const Value *V, Instruction *I, Function *F);
+  Value * getPoolHandle(const Value *V, Function *F, PA::FuncInfo &FI, bool collapsed = false);
+  void registerGlobalArraysWithGlobalPools(Module &M);
 #else
   void addLSChecks(Value *V, Instruction *I, Function *F);
-  void addLoadStoreChecks(Module &M);
   Value * getPoolHandle(const Value *V, Function *F);
 #endif  
 
