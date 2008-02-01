6ed7780ad1acd95e3a47da8c59f9c3133a00e1b7
criswell <criswell@91177308-0d34-0410-b5e6-96231b3b80d8>
2008-02-01 21:29:40 +0000

    Merged Dinakar's type checking code into mainline.
    git-svn-id: http://llvm.org/svn/llvm-project/safecode/trunk@87585 91177308-0d34-0410-b5e6-96231b3b80d8

---
+++ lib/ProofWrap/ProofWrap.cpp
@@ -0,0 +1,31 @@
+#include "llvm/Pass.h"
+#include "llvm/Function.h"
+#include "llvm/Constants.h"
+#include "ProofWrap/ProofWrap.h"
+
+using namespace llvm;
+
+namespace {
+  RegisterPass<ProofStrip> X("proofstrip", "Strip proof markers");
+}
+
+bool ProofStrip::runOnModule(Module& M) {
+  std::vector<const Type * > FTV;
+  FTV.push_back(Type::LongTy);
+  FunctionType* FT = FunctionType::get(Type::VoidTy, FTV, true);
+  Function* F = M.getFunction("llvm.proof.ptr", FT);
+  if (F) {
+    while (!F->use_empty()) {
+      User* U = *F->use_begin();
+      CallInst* CI = cast<CallInst>(U);
+      const Value* PV = CI->getOperand(1);
+      for (unsigned x = 2; x < CI->getNumOperands(); ++x)
+        setProof(CI->getOperand(x), PV);
+      CI->eraseFromParent();
+    }
+    return true;
+  }
+  return false;
+}
+
+//ProofPlace p;
