dfdab4a0524a3d659952a1c9479d822213be439d
criswell <criswell@91177308-0d34-0410-b5e6-96231b3b80d8>
2008-02-01 21:16:48 +0000

    Merged in see branch.
    git-svn-id: http://llvm.org/svn/llvm-project/safecode/trunk@87584 91177308-0d34-0410-b5e6-96231b3b80d8

--- lib/StackSafety/CheckStackPointer.cpp
+++ lib/StackSafety/CheckStackPointer.cpp
@@ -38,9 +38,9 @@ bool checkStackSafety::markReachableAllocasInt(DSNode *DSN, bool start) {
   for (unsigned i = 0, e = DSN->getSize(); i < e; i += DS::PointerSize)
     if (DSNode *DSNchild = DSN->getLink(i).getNode()) {
       if (reachableAllocaNodes.find(DSNchild) != reachableAllocaNodes.end()) {
-	continue;
+        continue;
       } else if (markReachableAllocasInt(DSNchild)) {
-	returnValue = returnValue || true;
+        returnValue = returnValue || true;
       }
     }
   return returnValue;
@@ -57,15 +57,18 @@ bool checkStackSafety::runOnModule(Module &M) {
     if (&F != MainFunc) {
       if (!F.isDeclaration()) {
         DSGraph &BUG = BUDS->getDSGraph(F);
-
+	
         // check if return value is a  pointers
         if (isa<PointerType>(F.getReturnType())) {
           //return value type is a pointer
-          for(inst_iterator ii = inst_begin(F), ie = inst_end(&F); ii != ie; ++ii) {
+          for (inst_iterator ii = inst_begin(F), ie = inst_end(&F);
+                             ii != ie;
+                             ++ii) {
             if (ReturnInst *RI = dyn_cast<ReturnInst>(&*ii)) {
               DSNode *DSN = BUG.getNodeForValue(RI).getNode();
               if (DSN && markReachableAllocas(DSN)) {
-                std::cerr << "Instruction : \n" << RI << "points to a stack location\n";
+                std::cerr << "Instruction : \n" << RI
+                          << "points to a stack location\n";
                 std::cerr << "In Function " << F.getName() << "\n";
                 return false;
               }
@@ -73,29 +76,29 @@ bool checkStackSafety::runOnModule(Module &M) {
           }
         }
     
-	Function::arg_iterator AI = F.arg_begin(), AE = F.arg_end();
-	for (; AI != AE; ++AI) {
-	  if (isa<PointerType>(AI->getType())) {
-	    DSNode *DSN = BUG.getNodeForValue(AI).getNode();
-	    if (markReachableAllocas(DSN,true)) {
-	      std::cerr << "Instruction : \n" << AI << "points to a stack location\n";
-	      std::cerr << "In Function " << F.getName() << "\n";
-	      
-	    }
-	  }
-	}
+        Function::arg_iterator AI = F.arg_begin(), AE = F.arg_end();
+        for (; AI != AE; ++AI) {
+          if (isa<PointerType>(AI->getType())) {
+            DSNode *DSN = BUG.getNodeForValue(AI).getNode();
+            if (markReachableAllocas(DSN,true)) {
+              std::cerr << "Instruction : \n" << AI
+                        << "points to a stack location\n";
+              std::cerr << "In Function " << F.getName() << "\n";
+            }
+          }
+        }
 	
-      // Also, mark allocas pointed to by globals
-	DSGraph::node_iterator DSNI = BUG.node_begin(), DSNE = BUG.node_end();
+        // Also, mark allocas pointed to by globals
+        DSGraph::node_iterator DSNI = BUG.node_begin(), DSNE = BUG.node_end();
 	
-	for (; DSNI != DSNE; ++DSNI) {
-	  if (DSNI->isGlobalNode()) {
-	    if (markReachableAllocas(DSNI)) {
-	      std::cerr << "Global points to a stack location\n";
-	      std::cerr << "In Function " << F.getName() << "\n";
-	    }
-	  }
-	}
+        for (; DSNI != DSNE; ++DSNI) {
+          if (DSNI->isGlobalNode()) {
+            if (markReachableAllocas(DSNI)) {
+              std::cerr << "Global points to a stack location\n";
+              std::cerr << "In Function " << F.getName() << "\n";
+            }
+          }
+        }
       }
     }
   }
