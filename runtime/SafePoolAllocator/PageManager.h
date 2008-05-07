5de7cb2e7df2e128c477e9e5cf4a90d5ff5f81b9
blau2 <blau2@91177308-0d34-0410-b5e6-96231b3b80d8>
2008-05-07 20:46:42 +0000

    runtime with dangling pointer detection
    git-svn-id: http://llvm.org/svn/llvm-project/safecode/trunk@87711 91177308-0d34-0410-b5e6-96231b3b80d8

--- runtime/SafePoolAllocator/PageManager.h
+++ runtime/SafePoolAllocator/PageManager.h
@@ -26,12 +26,31 @@ void InitializePageManager();
 ///
 extern "C" unsigned PageSize;
 
+extern unsigned PPageSize;
+
 /// AllocatePage - This function returns a chunk of memory with size and
 /// alignment specified by getPageSize().
 void *AllocatePage();
 
-/// AllocateNPages - 
+// RemapPage - This function is used by the dangling pool allocator in order
+//				to remap canonical page to shadow page.
+void * RemapPage(void* va, unsigned NumByte);
+
+/// AllocateNPages - Allocates Num number of pages
 void *AllocateNPages(unsigned Num);
+
+// MProtectPage - Protects Page passed in by argument, raising an exception
+//					or traps at future access to Page
+void MProtectPage(void * Page, unsigned NumPages);
+
+// ProtectShadowPage - Protects shadow page that begins at beginAddr, spanning
+//						over NumPages
+void ProtectShadowPage(void * beginPage, unsigned NumPPages);
+
+// UnprotectShadowPage - Unprotects the shadow page in the event of fault when
+//							accessing protected shadow page in order to
+//							resume execution
+void UnprotectShadowPage(void * beginPage, unsigned NumPPage);
 
 /// FreePage - This function returns the specified page to the pagemanager for
 /// future allocation.
