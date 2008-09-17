ad1ab49bb61e18b5cbd2ade8e13e9fb34cf9cd34
criswell <criswell@91177308-0d34-0410-b5e6-96231b3b80d8>
2008-09-17 18:49:56 +0000

    Header file for the new Fault Injector pass.
    git-svn-id: http://llvm.org/svn/llvm-project/safecode/trunk@87906 91177308-0d34-0410-b5e6-96231b3b80d8

---
+++ include/FaultInjector.h
@@ -0,0 +1,44 @@
+//===- FaultInjector.h - Insert faults into programs -------------------------//
+// 
+//                          The SAFECode Compiler 
+//
+// This file was developed by the LLVM research group and is distributed under
+// the University of Illinois Open Source License. See LICENSE.TXT for details.
+// 
+//===----------------------------------------------------------------------===//
+//
+// This pass transforms injects faults into a program.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef FAULT_INJECTOR_H
+#define FAULT_INJECTOR_H
+
+#include "safecode/Config/config.h"
+#include "dsa/DataStructure.h"
+#include "llvm/Pass.h"
+
+namespace llvm {
+
+  struct FaultInjector : public ModulePass {
+    public :
+      static char ID;
+      FaultInjector () : ModulePass ((intptr_t) &ID) {
+        return;
+      }
+
+      const char *getPassName() const { return "Fault Injector Pass"; }
+      virtual bool runOnModule(Module &M);
+      virtual void getAnalysisUsage(AnalysisUsage &AU) const {
+        AU.addRequired<TDDataStructures>();
+      };
+
+    private :
+      // Private variables
+      TDDataStructures * TDPass;
+
+      // Private methods
+      bool insertDanglingPointers (Function & F);
+  };
+}
+#endif
