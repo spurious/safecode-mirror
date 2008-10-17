65e72a87f4cf25cb925c3041888a628eb56ebd99
criswell <criswell@91177308-0d34-0410-b5e6-96231b3b80d8>
2008-10-17 19:46:03 +0000

    Improved formatting. Added header comment. Re-ordered LLVM includes to be in alphabetical order. No functionality changes.
    git-svn-id: http://llvm.org/svn/llvm-project/safecode/trunk@88054 91177308-0d34-0410-b5e6-96231b3b80d8

--- include/BottomUpCallGraph.h
+++ include/BottomUpCallGraph.h
@@ -1,47 +1,57 @@
+//===- BottomUpCallGraph.cpp - -----------------------------------------------//
+// 
+//                          The SAFECode Compiler 
+//
+// This file was developed by the LLVM research group and is distributed under
+// the University of Illinois Open Source License. See LICENSE.TXT for details.
+// 
+//===----------------------------------------------------------------------===//
+//
+//===----------------------------------------------------------------------===//
+
 #ifndef BOTTOMUP_CALLGRAPH_H
 #define BOTTOMUP_CALLGRAPH_H
 
 #include "dsa/DataStructure.h"
 #include "dsa/DSSupport.h"
 #include "llvm/ADT/hash_map.h"
-#include "llvm/Pass.h"
 #include "llvm/Function.h"
 #include "llvm/Module.h"
+#include "llvm/Pass.h"
 
 #include <set> 
 
 namespace llvm {
   struct BottomUpCallGraph : public ModulePass {
     private:
-    // OneCalledFunction - For each indirect function call, we keep track of one
-    // the DSNode and the corresponding Call Instruction
-    typedef hash_multimap<DSNode*, CallSite> CalleeNodeCallSiteMapTy;
-    CalleeNodeCallSiteMapTy CalleeNodeCallSiteMap ;
-    std::vector<Function *> Stack;
-    std::set<Function *> Visited;
-    void figureOutSCCs(Module &M);
-    void visit(Function *f);
+      // OneCalledFunction - For each indirect function call, we keep track of
+      // one the DSNode and the corresponding Call Instruction
+      typedef hash_multimap<DSNode*, CallSite> CalleeNodeCallSiteMapTy;
+      CalleeNodeCallSiteMapTy CalleeNodeCallSiteMap ;
+      std::vector<Function *> Stack;
+      std::set<Function *> Visited;
+      void figureOutSCCs(Module &M);
+      void visit(Function *f);
 
     public:
-    
-    static char ID;
-    BottomUpCallGraph () : ModulePass ((intptr_t) &ID) {}
-    const char *getPassName() const { return "Bottom-Up Call Graph"; }
+      static char ID;
+      BottomUpCallGraph () : ModulePass ((intptr_t) &ID) {}
+      const char *getPassName() const { return "Bottom-Up Call Graph"; }
 
-    //This keeps the map of a function and its call sites in all the callers
-    //including the indirectly called sites
-    std::map<const Function *, std::vector<CallSite> > FuncCallSiteMap;
-    std::set<Function *> SccList; //Fns involved in Sccs
-    
-    bool isInSCC(Function *f) {
-      return (SccList.find(f) != SccList.end());
-    }
-    
-    virtual void getAnalysisUsage(AnalysisUsage &AU) const {
-      AU.addRequiredTransitive<CompleteBUDataStructures>();
-      AU.setPreservesAll();	
-    }
-    virtual bool runOnModule(Module&M);
+      // This keeps the map of a function and its call sites in all the callers
+      // including the indirectly called sites
+      std::map<const Function *, std::vector<CallSite> > FuncCallSiteMap;
+      std::set<Function *> SccList; //Fns involved in Sccs
+
+      bool isInSCC(Function *f) {
+        return (SccList.find(f) != SccList.end());
+      }
+
+      virtual void getAnalysisUsage(AnalysisUsage &AU) const {
+        AU.addRequiredTransitive<CompleteBUDataStructures>();
+        AU.setPreservesAll();	
+      }
+      virtual bool runOnModule(Module&M);
   };
 }
 #endif
