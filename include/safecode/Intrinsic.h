aa966c3c8d1717be9cdf69ad1b0db5dc37ce27e2
criswell <criswell@91177308-0d34-0410-b5e6-96231b3b80d8>
2009-01-05 17:03:03 +0000

    Formatting and comment improvements. No functionality changes.
    git-svn-id: http://llvm.org/svn/llvm-project/safecode/trunk@88197 91177308-0d34-0410-b5e6-96231b3b80d8

--- include/safecode/Intrinsic.h
+++ include/safecode/Intrinsic.h
@@ -1,4 +1,4 @@
-//===- Intrinsic.h - Insert declaration of SAFECode intrinsic to bc files --------------//
+//===- Intrinsic.h - Insert declaration of SAFECode intrinsic to bc files ----//
 // 
 //                          The SAFECode Compiler 
 //
@@ -7,9 +7,9 @@
 // 
 //===----------------------------------------------------------------------===//
 //
-// This file implements a module plass to insert declaration of
-// SAFECode intrinsic to the bitcode file. It also provides interfaces
-// for latter passes to use these intrinsics.
+// This file implements a module pass to insert declarations of the SAFECode
+// intrinsics into the bitcode file. It also provides interfaces for later
+// passes which use these intrinsics.
 //
 //===----------------------------------------------------------------------===//
 
@@ -26,37 +26,43 @@
 NAMESPACE_SC_BEGIN
 
 class InsertSCIntrinsic : public llvm::ModulePass {
- public:
-  static char ID;
-  typedef enum IntrinsicType {
-    SC_INTRINSIC_NO_OP,
-    SC_INTRINSIC_CHECK,
-    SC_INTRINSIC_OOB,
-    SC_INTRINSIC_POOL_CONTROL,
-    SC_INTRINSIC_COUNT
-  } IntrinsicType;
+  public:
+    static char ID;
+    typedef enum IntrinsicType {
+      SC_INTRINSIC_NO_OP,
+      SC_INTRINSIC_CHECK,
+      SC_INTRINSIC_OOB,
+      SC_INTRINSIC_POOL_CONTROL,
+      SC_INTRINSIC_COUNT
+    } IntrinsicType;
 
-  typedef struct IntrinsicInfo {
-    IntrinsicType type;
-    llvm::Function * F;
-  } IntrinsicInfoTy;
+    typedef struct IntrinsicInfo {
+      IntrinsicType type;
+      llvm::Function * F;
+    } IntrinsicInfoTy;
 
- InsertSCIntrinsic() : llvm::ModulePass((intptr_t)&ID), currentModule(NULL) {}
-  virtual ~InsertSCIntrinsic() {}
-  virtual bool runOnModule(llvm::Module & M);
-  virtual const char * getPassName() const { return "Insert declaration of SAFECode Intrinsic"; }
-  void addIntrinsic(IntrinsicType type, const std::string & name, llvm::FunctionType * FTy);
-  const IntrinsicInfoTy & getIntrinsic(const std::string & name) const;
-  bool isSCIntrinsic(llvm::Value * inst) const;
-  bool isCheckingIntrinsic(llvm::Value * inst) const;
-  virtual void getAnalysisUsage(llvm::AnalysisUsage &AU) const {
-    AU.setPreservesCFG();
-    AU.setPreservesAll();
-  }
- private:
-  llvm::Module * currentModule;
-  std::map<llvm::Function *, IntrinsicInfoTy> intrinsicFuncMap;
-  std::map<std::string, IntrinsicInfoTy> intrinsicNameMap;
+    InsertSCIntrinsic() : llvm::ModulePass((intptr_t)&ID),
+                          currentModule(NULL) {}
+    virtual ~InsertSCIntrinsic() {}
+    virtual bool runOnModule(llvm::Module & M);
+    virtual const char * getPassName() const {
+      return "Insert declaration of SAFECode Intrinsic";
+    }
+    void addIntrinsic (IntrinsicType type,
+                       const std::string & name,
+                       llvm::FunctionType * FTy);
+    const IntrinsicInfoTy & getIntrinsic(const std::string & name) const;
+    bool isSCIntrinsic(llvm::Value * inst) const;
+    bool isCheckingIntrinsic(llvm::Value * inst) const;
+    virtual void getAnalysisUsage(llvm::AnalysisUsage &AU) const {
+      AU.setPreservesCFG();
+      AU.setPreservesAll();
+    }
+
+  private:
+    llvm::Module * currentModule;
+    std::map<llvm::Function *, IntrinsicInfoTy> intrinsicFuncMap;
+    std::map<std::string, IntrinsicInfoTy> intrinsicNameMap;
 };
 
 NAMESPACE_SC_END
