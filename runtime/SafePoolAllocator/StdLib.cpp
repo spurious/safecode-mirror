da936a454e43f9804966477cd404726f1bbfbb6f
bglin2 <bglin2@91177308-0d34-0410-b5e6-96231b3b80d8>
2009-02-11 04:32:31 +0000

    1. Created StdLib.cpp to provide necessary runtime functions. 2. Added two variations of strnlen (one more portable, one more optimized).
    git-svn-id: http://llvm.org/svn/llvm-project/safecode/trunk@88230 91177308-0d34-0410-b5e6-96231b3b80d8

---
+++ runtime/SafePoolAllocator/StdLib.cpp
@@ -0,0 +1,28 @@
+//===------- StdLib.cpp - CStdLib transform pass runtime functions --------===//
+// 
+//                     The LLVM Compiler Infrastructure
+//
+// This file was developed by the LLVM research group and is distributed under
+// the University of Illinois Open Source License. See LICENSE.TXT for details.
+// 
+//===----------------------------------------------------------------------===//
+//
+// This file provides all external functions included by the CStdLib pass.
+//
+//===----------------------------------------------------------------------===//
+
+#include "PoolAllocator.h"
+
+extern "C" {
+	size_t strnlen(const char *s, size_t maxlen) {
+		size_t i;
+		for (i = 0; i < maxlen && s[i]; ++i)
+			;
+		return i;
+	}
+
+	size_t strnlen_opt(const char *s, size_t maxlen) {
+		const char *end = (const char *)memchr(s, '\0', maxlen);
+		return (end ? end - s : maxlen);
+	}
+}
