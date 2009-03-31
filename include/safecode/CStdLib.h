e027299ada08f390a5205cbb0b9e306109bacd4b
bglin2 <bglin2@91177308-0d34-0410-b5e6-96231b3b80d8>
2009-03-31 02:42:55 +0000

    1. Cleaned up C standard library call transform pass by adding comments and fixing formatting issues. 2. Used correct NAMESPACE_SC macros. 3. Included necessary dependency passes for getAnalysisUsage().
    git-svn-id: http://llvm.org/svn/llvm-project/safecode/trunk@88321 91177308-0d34-0410-b5e6-96231b3b80d8

--- include/safecode/CStdLib.h
+++ include/safecode/CStdLib.h
@@ -1,3 +1,17 @@
+//===------------ CStdLib.h - Secure C standard library calls -------------===//
+// 
+//                          The SAFECode Compiler 
+//
+// This file was developed by the LLVM research group and is distributed under
+// the University of Illinois Open Source License. See LICENSE.TXT for details.
+// 
+//===----------------------------------------------------------------------===//
+//
+// This pass finds all calls to functions in the C standard library and
+// transforms them to a more secure form.
+//
+//===----------------------------------------------------------------------===//
+
 #ifndef CSTDLIB_H
 #define CSTDLIB_H
 
@@ -5,44 +19,50 @@
 #include "llvm/Constants.h"
 #include "llvm/DerivedTypes.h"
 #include "llvm/Function.h"
+#include "llvm/Instructions.h"
 #include "llvm/Pass.h"
 #include "llvm/ADT/Statistic.h"
 #include "llvm/Support/Debug.h"
 #include "llvm/Support/InstVisitor.h"
 
+#include "safecode/PoolHandles.h"
+#include "safecode/SAFECode.h"
+
 #include <algorithm>
 #include <vector>
 
-#include <fcntl.h>
-
 using namespace llvm;
 
+NAMESPACE_SC_BEGIN
+
 // Statistics counters
-/*
-STATISTIC(stat_missed_strcpy, "Total strcpy() calls missed");
-*/
+STATISTIC(stat_transform_strcpy, "Total strcpy() calls transformed");
 
-// From KTransform.h
-namespace llvm {
-	ModulePass * createStringTransformPass();
-}
+/**
+ * Pass that secures C standard library string calls via transforms
+ */
+class StringTransform : public ModulePass {
+private:
+  bool strcpyTransform(Module &M);
 
-namespace {
-	class StringTransform : public ModulePass {
-	private:
-                bool strcpyTransform(Module &M);
+public:
+  static char ID;
+  StringTransform() : ModulePass((intptr_t)&ID) {}
+  virtual bool runOnModule(Module &M);
 
-	public:
-		static char ID;
-		StringTransform() : ModulePass((intptr_t)&ID) {}
-                virtual bool runOnModule(Module &M);
+  virtual void getAnalysisUsage(AnalysisUsage &AU) const {
+    // Pretend that we don't modify anything
+    AU.setPreservesAll();
 
-		virtual void getAnalysisUsage(AnalysisUsage &AU) const {
-			AU.setPreservesAll();
-		}
-		virtual void print(std::ostream &O, const Module *M) const {}
-	};
+    // We require these passes to get information on pool handles
+    AU.addRequired<DSNodePass>();
+    AU.addRequired<PoolAllocateGroup>();
+    AU.addRequired<TDDataStructures>();
+  }
 
-}
+  virtual void print(std::ostream &O, const Module *M) const {}
+};
+
+NAMESPACE_SC_END
 
 #endif
--- lib/CStdLib/String.cpp
+++ lib/CStdLib/String.cpp
@@ -14,17 +14,24 @@
 
 #include "safecode/CStdLib.h"
 
-static RegisterPass<StringTransform> X("string_transform", "Secure C standard string library calls");
+NAMESPACE_SC_BEGIN
 
-ModulePass *llvm::createStringTransformPass() { return new StringTransform(); }
+static RegisterPass<StringTransform> X("string_transform",
+                                       "Secure C standard string library calls");
 
+/**
+ * Entry point for the LLVM pass that transforms C standard string library calls
+ *
+ * @param	M	Module to scan
+ * @return	Whether we modified the module
+ */
 bool StringTransform::runOnModule(Module &M) {
-	// Flags whether we modified the module
-	bool modified = false;
+  // Flags whether we modified the module
+  bool modified = false;
 
-	modified |= strcpyTransform(M);
+  modified |= strcpyTransform(M);
 
-	return modified;
+  return modified;
 }
 
 /**
@@ -34,57 +41,61 @@ bool StringTransform::runOnModule(Module &M) {
  * @return	Whether we modified the module
  */
 bool StringTransform::strcpyTransform(Module &M) {
-	// Flags whether we modified the module
-	bool modified = false;
+  // Flags whether we modified the module
+  bool modified = false;
 
-	std::vector<const Type *> strncpyParameters;
-	strncpyParameters.push_back(PointerType::getUnqual(Type::Int8Ty));
-	strncpyParameters.push_back(PointerType::getUnqual(Type::Int8Ty));
-	strncpyParameters.push_back(PointerType::getUnqual(Type::Int32Ty)); // size_t
+  std::vector<const Type *> strncpyParameters;
+  strncpyParameters.push_back(PointerType::getUnqual(Type::Int8Ty));
+  strncpyParameters.push_back(PointerType::getUnqual(Type::Int8Ty));
+  strncpyParameters.push_back(PointerType::getUnqual(Type::Int32Ty)); // size_t
 
-	Function * strcpyFunc = M.getFunction("strcpy");
-	if (!strcpyFunc)
-		return modified;
-	const Type * strncpyRT = strcpyFunc->getReturnType();
-	FunctionType * strncpyFT = FunctionType::get(strncpyRT, strncpyParameters, false);
-	Constant * strncpyFunction = M.getOrInsertFunction("strncpy", strncpyFT);
+  Function * strcpyFunc = M.getFunction("strcpy");
+  if (!strcpyFunc)
+    return modified;
+  const Type * strncpyRT = strcpyFunc->getReturnType();
+  FunctionType * strncpyFT = FunctionType::get(strncpyRT, strncpyParameters, false);
+  Constant * strncpyFunction = M.getOrInsertFunction("strncpy", strncpyFT);
 
-	// Scan through the module and replace strcpy() with strncpy().
-	for (Module::iterator F=M.begin(); F != M.end(); ++F) {
-		for (Function::iterator BB = F->begin(); BB != F->end(); ++BB) {
-			for (BasicBlock::iterator I = BB->begin(); I != BB->end(); ++I) {
-				int bounds = 0; // getBounds()
+  // Scan through the module and replace strcpy() with strncpy().
+  for (Module::iterator F=M.begin(); F != M.end(); ++F) {
+    for (Function::iterator BB = F->begin(); BB != F->end(); ++BB) {
+      for (BasicBlock::iterator I = BB->begin(); I != BB->end(); ++I) {
+        int bounds = 0; // getBounds()
 
-				// If this is not a call or invoke instruction, just skip it.
-				if (!(isa<CallInst>(I) || isa<InvokeInst>(I)))
-					continue;
+        // If this is not a call or invoke instruction, just skip it.
+        if (!(isa<CallInst>(I) || isa<InvokeInst>(I)))
+          continue;
 
-				// Otherwise, figure out if this is a direct call to strcpy().
-				CallInst * CI = dyn_cast<CallInst>(I);
-				Function * CalledFunc = CI->getCalledFunction();
+        // Otherwise, figure out if this is a direct call to strcpy().
+        CallInst * CI = dyn_cast<CallInst>(I);
+        Function * CalledFunc = CI->getCalledFunction();
 
-				if (CalledFunc == 0 || !CalledFunc->hasName())
-					continue;
+        if (CalledFunc == 0 || !CalledFunc->hasName())
+          continue;
 
-				if (CalledFunc->getName() != "strcpy")
-					continue;
+        if (CalledFunc->getName() != "strcpy")
+          continue;
 
-				// Create a strncpy() call.
-				std::vector<Value *> Params;
-				Params.push_back( ConstantInt::get(Type::Int32Ty, bounds) );
-				Params.push_back( CI->getOperand(1) );
-				CallInst * strncpyCallInst = CallInst::Create(strncpyFunction, Params.begin(), Params.end(), "strings", CI);
+        // Create a strncpy() call.
+        std::vector<Value *> Params;
+        Params.push_back( ConstantInt::get(Type::Int32Ty, bounds) );
+        Params.push_back( CI->getOperand(1) );
+        CallInst * strncpyCallInst = CallInst::Create(strncpyFunction, Params.begin(), Params.end(), "strings", CI);
 
-				// Replace all of the uses of the strcpy() call with the new strncpy() call.
-				CI->replaceAllUsesWith(strncpyCallInst);
-				CI->eraseFromParent();
+        // Replace all of the uses of the strcpy() call with the new strncpy() call.
+        CI->replaceAllUsesWith(strncpyCallInst);
+        CI->eraseFromParent();
 
-				// Mark the module as modified and continue to the next strcpy() call.
-				modified = true;
-			}
-		}
-	}
+        // Record the transform.
+        ++stat_transform_strcpy;
 
-	return modified;
+        // Mark the module as modified and continue to the next strcpy() call.
+        modified = true;
+      }
+    }
+  }
+
+  return modified;
 }
 
+NAMESPACE_SC_END
