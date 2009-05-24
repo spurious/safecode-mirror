101cabbb54a1fbc5c6bbc0a6b4431d214c04c2d6
mai4 <mai4@91177308-0d34-0410-b5e6-96231b3b80d8>
2009-05-24 03:17:54 +0000

    Add missing file
    git-svn-id: http://llvm.org/svn/llvm-project/safecode/trunk@88363 91177308-0d34-0410-b5e6-96231b3b80d8

---
+++ include/safecode/SAFECodeConfig.h
@@ -0,0 +1,33 @@
+//===- SAFECodeConfig.h -----------------------------------------*- C++ -*----//
+// 
+//                          The SAFECode Compiler 
+//
+// This file was developed by the LLVM research group and is distributed under
+// the University of Illinois Open Source License. See LICENSE.TXT for details.
+// 
+//===----------------------------------------------------------------------===//
+//
+// Parse and record all configuration parameters required by SAFECode.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef _SAFECODE_CODE_CONFIG_H_
+#define _SAFECODE_CODE_CONFIG_H_
+
+#include "safecode/SAFECode.h"
+
+NAMESPACE_SC_BEGIN
+
+struct SAFECodeConfiguration {
+  bool DanglingPointerChecks;
+  bool RewriteOOB;
+  bool TerminateOnErrors;
+
+  static SAFECodeConfiguration * create();
+private:
+  SAFECodeConfiguration();
+};
+
+extern SAFECodeConfiguration * SCConfig;
+NAMESPACE_SC_END
+#endif
