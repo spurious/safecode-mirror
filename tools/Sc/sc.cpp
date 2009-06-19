b8f658649f79bc36e93f14a002343c37211026f0
mai4 <mai4@91177308-0d34-0410-b5e6-96231b3b80d8>
2009-06-19 05:27:26 +0000

    Fix naming issues for poolcalloc_debug().
    git-svn-id: http://llvm.org/svn/llvm-project/safecode/trunk@88427 91177308-0d34-0410-b5e6-96231b3b80d8

--- tools/Sc/sc.cpp
+++ tools/Sc/sc.cpp
@@ -478,12 +478,13 @@ static void addLowerIntrinsicPass(PassManager & Passes, CheckingRuntimeType type
       {"sc.exactcheck2_debug","exactcheck2_debug"},
       {"sc.pool_argvregister", "__sc_dbg_poolargvregister"},
 
-      {"poolalloc_debug",        "__sc_dbg_src_poolalloc"},
+      {"poolalloc_debug",       "__sc_dbg_src_poolalloc"},
       {"poolfree_debug",        "__sc_dbg_src_poolfree"},
 
       // These functions register objects in the splay trees
-      {"poolcalloc",        "__sc_dbg_poolcalloc"},
-      {"poolrealloc",       "__sc_dbg_poolrealloc"},
+      {"poolcalloc_debug",      "__sc_dbg_src_poolcalloc"},
+      {"poolcalloc",            "__sc_dbg_poolcalloc"},
+      {"poolrealloc",           "__sc_dbg_poolrealloc"},
     };
 
 
