3de75951c9ccfbbdcf8abad66e515a81e5a6ce70
criswell <criswell@91177308-0d34-0410-b5e6-96231b3b80d8>
2009-07-07 15:58:16 +0000

    Header file for pass that makes strings non-constant.
    git-svn-id: http://llvm.org/svn/llvm-project/safecode/trunk@88490 91177308-0d34-0410-b5e6-96231b3b80d8

---
+++ include/safecode/BreakConstantStrings.h
@@ -0,0 +1,53 @@
+//===- BreakConstantStrings.h - Make global string constants non-constant - --//
+// 
+//                          The SAFECode Compiler 
+//
+// This file was developed by the LLVM research group and is distributed under
+// the University of Illinois Open Source License. See LICENSE.TXT for details.
+// 
+//===----------------------------------------------------------------------===//
+//
+// This pass changes all GEP constant expressions into GEP instructions.  This
+// permits the rest of SAFECode to put run-time checks on them if necessary.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef BREAKCONSTANTSTRINGS_H
+#define BREAKCONSTANTSTRINGS_H
+
+#include "llvm/Analysis/Dominators.h"
+#include "llvm/Module.h"
+#include "llvm/Pass.h"
+
+#include "safecode/SAFECode.h"
+
+using namespace llvm;
+
+NAMESPACE_SC_BEGIN
+
+//
+// Pass: BreakConstantStrings
+//
+// Description:
+//  This pass modifies an LLVM Module so that strings are not constant.
+//
+struct BreakConstantStrings : public ModulePass {
+  private:
+    // Private methods
+
+    // Private variables
+
+  public:
+    static char ID;
+    BreakConstantStrings() : ModulePass((intptr_t)(&ID)) {}
+    const char *getPassName() const {return "Make Global Strings Non-constant";}
+    virtual bool runOnModule (Module & M);
+    virtual void getAnalysisUsage(AnalysisUsage &AU) const {
+      // This pass does not modify the control-flow graph of the function
+      AU.setPreservesCFG();
+    }
+};
+
+NAMESPACE_SC_END
+
+#endif
