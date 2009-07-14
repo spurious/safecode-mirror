b7e4506dbeb7613f67e8d18b8bba67ebefa5830b
criswell <criswell@91177308-0d34-0410-b5e6-96231b3b80d8>
2009-07-14 15:46:20 +0000

    Modified code to strip casts before checking whether the pointer has any uses.  The cast instruction may have no uses, but the original pointer might. Added statistic to count the number of run-time checks removed. Added comments.
    git-svn-id: http://llvm.org/svn/llvm-project/safecode/trunk@88531 91177308-0d34-0410-b5e6-96231b3b80d8

--- lib/OptimizeChecks/UnusedCheckElimination.cpp
+++ lib/OptimizeChecks/UnusedCheckElimination.cpp
@@ -36,8 +36,13 @@ NAMESPACE_SC_BEGIN
 
 char UnusedCheckElimination::ID = 0;
 
-static RegisterPass<UnusedCheckElimination> X ("unused-check-elim", "Unused Check elimination");
+namespace {
+  // Pass Statistics
+  STATISTIC (Removed, "Number of checks on unused pointers removed");
+}
 
+static RegisterPass<UnusedCheckElimination>
+X ("unused-check-elim", "Unused Check elimination");
 
 bool
 UnusedCheckElimination::runOnModule (Module & M) {
@@ -47,20 +52,52 @@ UnusedCheckElimination::runOnModule (Module & M) {
   unusedChecks.clear();
   intrinsic = &getAnalysis<InsertSCIntrinsic>();
 
-  InsertSCIntrinsic::intrinsic_const_iterator i, e;
-  for (i = intrinsic->intrinsic_begin(), e = intrinsic->intrinsic_end(); i != e; ++i) {
+  //
+  // Scan through the use/def chains of all the run-time checks.  If the
+  // pointer being checked is never used, then eliminate the check.
+  //
+  InsertSCIntrinsic::intrinsic_const_iterator i = intrinsic->intrinsic_begin();
+  InsertSCIntrinsic::intrinsic_const_iterator e = intrinsic->intrinsic_end();
+  for (; i != e; ++i) {
     if (i->flag & (InsertSCIntrinsic::SC_INTRINSIC_CHECK | InsertSCIntrinsic::SC_INTRINSIC_OOB)) {
-      for (Value::use_iterator I = i->F->use_begin(), E = i->F->use_end(); I != E; ++I) {
+      for (Value::use_iterator I = i->F->use_begin(), E = i->F->use_end();
+           I != E;
+           ++I) {
+        //
+        // Get the pointer that the run-time check is checking.  Strip off the
+        // casts because the cast may have no uses but the pointer it comes
+        // from may have uses (other than the casts).
+        //
         CallInst * CI = cast<CallInst>(*I);
-        if (intrinsic->getValuePointer(CI)->use_empty()) unusedChecks.push_back(CI);
+        Value * CheckedPointer = intrinsic->getValuePointer(CI)
+                                           ->stripPointerCasts();
+
+        //
+        // If the checked pointer has no uses, schedule the run-time check for
+        // deletion.
+        //
+        if (CheckedPointer->use_empty()) unusedChecks.push_back(CI);
       }
     }
   }
 
+  //
+  // Delete all unneeded run-time checks.
+  //
   DeleteItself op;
   std::for_each(unusedChecks.begin(), unusedChecks.end(), op);
 
+  //
+  // Record whether we modified the program and update the statistics.  Note
+  // that we update the statistics because this pass could be run multipe
+  // times, and we want the total number of eliminated checks.
+  //
   bool modified = unusedChecks.size() > 0;
+  Removed += unusedChecks.size();
+
+  //
+  // Free memory and finish the pass.
+  //
   unusedChecks.clear();
   return modified;
 }
