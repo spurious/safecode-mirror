7277574e39223fb8d6c0192772e400ea74e13a8e
criswell <criswell@91177308-0d34-0410-b5e6-96231b3b80d8>
2009-09-15 16:23:59 +0000

    Start providing more human-readable reports of what the error is.
    git-svn-id: http://llvm.org/svn/llvm-project/safecode/trunk@88581 91177308-0d34-0410-b5e6-96231b3b80d8

--- runtime/DebugRuntime/Report.cpp
+++ runtime/DebugRuntime/Report.cpp
@@ -24,10 +24,54 @@ ViolationInfo::~ViolationInfo() {}
 
 void
 ViolationInfo::print(std::ostream & OS) const {
+  //
+  // Print a single line report describing the error.  This is used, I believe,
+  // by the automatic testing infrastructure scripts to determine if a safety
+  // violation was correctly detected.
+  //
   OS << std::showbase << std::hex 
      << "SAFECode:Violation Type " << this->type << " "
      << "when accessing  " << this->faultPtr << " "
      << "at IP=" << this->faultPC << "\n";
+
+  //
+  // Determine which descriptive string to use to describe the error.
+  //
+  const char * typestring;
+  switch (type) {
+    case FAULT_DANGLING_PTR:
+      typestring = "Dangling Pointer Error";
+      break;
+
+    case FAULT_DOUBLE_FREE:
+      typestring = "Double Free Error";
+      break;
+
+    case FAULT_OUT_OF_BOUNDS:
+      typestring = "Out of Bounds Error";
+      break;
+
+    case FAULT_LOAD_STORE:
+      typestring = "Load/Store Error";
+      break;
+
+    case FAULT_ALIGN:
+      typestring = "Alignment Error";
+      break;
+
+    default:
+      typestring = "Unknown Error";
+      break;
+  }
+
+  //
+  // Now print a more human readable version of the error.
+  //
+  OS << "\n";
+  OS << "=======+++++++    SAFECODE RUNTIME ALERT +++++++=======\n";
+  OS << "= Error type                            :\t" << typestring << "\n";
+  OS << "= Program counter                       :\t" << this->faultPC << "\n";
+  OS << "= Faulting pointer                      :\t" << this->faultPtr << "\n";
 }
 
 void
