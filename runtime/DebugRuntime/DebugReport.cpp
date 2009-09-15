3d05d0c32e2e39cce34ca68adaf4875972a9c8a8
criswell <criswell@91177308-0d34-0410-b5e6-96231b3b80d8>
2009-09-15 16:24:46 +0000

    Print out more readable reports on errors. Added code for the alignment error class.
    git-svn-id: http://llvm.org/svn/llvm-project/safecode/trunk@88582 91177308-0d34-0410-b5e6-96231b3b80d8

--- runtime/DebugRuntime/DebugReport.cpp
+++ runtime/DebugRuntime/DebugReport.cpp
@@ -19,11 +19,26 @@ NAMESPACE_SC_BEGIN
 
 void
 DebugViolationInfo::print(std::ostream & OS) const {
+  //
+  // Print out the regular error information.
+  //
   ViolationInfo::print(OS);
-  OS << "Fault PC Source: " << 
-    (this->SourceFile ? this->SourceFile : "<unknown>") <<
-    ":" << std::dec << this->lineNo << "\n"
-		<< "Pool Handle:" << this->PoolHandle << "\n";
+
+  //
+  // Print the source filename and line number.
+  //
+  OS << "= Fault PC Source                       :\t"
+     << (this->SourceFile ? this->SourceFile : "<unknown>")
+     << ":" << std::dec << this->lineNo << "\n";
+
+  //
+  // Print the pool handle.
+  //
+  OS << "= Pool Handle                           :\t" << this->PoolHandle << "\n";
+
+  //
+  // Print the debug metata.
+  //
   if (dbgMetaData) {
     dbgMetaData->print(OS);
   }
@@ -31,22 +46,45 @@ DebugViolationInfo::print(std::ostream & OS) const {
 
 void
 OutOfBoundsViolation::print(std::ostream & OS) const {
+  //
+  // Print out the regular error information.
+  //
   DebugViolationInfo::print(OS);
-  OS << std::showbase << std::hex
-     << "Object start:" << this->objStart << "\n"
-     << "Object length:" << this->objLen << "\n";
+
+  //
+  // Print information on the start and end locations of the object.
+  //
+  OS << "= Object start                          :\t" 
+     << std::showbase << std::hex << this->objStart << "\n"
+     << "= Object length                         :\t"
+     << this->objLen << "\n";
+}
+
+void
+AlignmentViolation::print(std::ostream & OS) const {
+  //
+  // Print out the regular error information.
+  //
+  OutOfBoundsViolation::print(OS);
+
+  //
+  // Print information on the alignment requirements for the object.
+  //
+  OS << "= Alignment                             :\t" 
+     << std::showbase << std::hex << this->alignment << "\n";
 }
 
 void
 DebugMetaData::print(std::ostream & OS) const {
   OS << std::showbase
-     << "Object address:" << std::hex << this->canonAddr << "\n"
-     << "Object allocated at PC:" << std::hex << this->allocPC << "\n"
-     << "Source File: " << (this->SourceFile ? this->SourceFile : "<unknown>")
+     << "= Object address:" << std::hex << this->canonAddr << "\n"
+     << "= Object allocated at PC:" << std::hex << this->allocPC << "\n";
+  OS << "= Source File: " << (this->SourceFile ? this->SourceFile : "<unknown>")
      << ":" << std::dec << this->lineno << "\n"
-     << "Object allocation generation number:" << std::dec << this->allocID << "\n"
-     << "Object freed at PC:" << std::hex << this->freePC << "\n"
-     << "Object free generation number:" << std::dec << this->freeID << "\n";
+     << "= Object allocation generation number:" << std::dec
+     << this->allocID << "\n"
+     << "= Object freed at PC:" << std::hex << this->freePC << "\n"
+     << "= Object free generation number:" << std::dec << this->freeID << "\n";
 }
 
 NAMESPACE_SC_END
