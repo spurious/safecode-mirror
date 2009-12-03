9fb40e38a934be16100778c628c57f763a471c73
criswell <criswell@91177308-0d34-0410-b5e6-96231b3b80d8>
2009-12-03 18:25:11 +0000

    Don't use builtin_return_address(1).  If the run-time functions are lined, then looking up beyond a single strack frame can cause errors to occur.
    git-svn-id: http://llvm.org/svn/llvm-project/safecode/trunk@90464 91177308-0d34-0410-b5e6-96231b3b80d8

--- runtime/DebugRuntime/PoolAllocatorBitMask.cpp
+++ runtime/DebugRuntime/PoolAllocatorBitMask.cpp
@@ -325,7 +325,7 @@ _internal_poolregister (DebugPoolTy *Pool,
   debugmetadataPtr = createPtrMetaData (allocID,
                                         0,
                                         allocationType,
-                                        __builtin_return_address(1),
+                                        __builtin_return_address(0),
                                         0,
                                         getCanonicalPtr(allocaptr),
                                         SourceFile, lineno);
@@ -566,7 +566,7 @@ _internal_poolunregister (DebugPoolTy *Pool,
   if (!found) {
     DebugViolationInfo v;
     v.type = DebugViolationInfo::FAULT_INVALID_FREE,
-      v.faultPC = __builtin_return_address(1),
+      v.faultPC = __builtin_return_address(0),
       v.faultPtr = allocaptr;
       v.PoolHandle = Pool;
       v.dbgMetaData = debugmetadataptr;
@@ -581,7 +581,7 @@ _internal_poolunregister (DebugPoolTy *Pool,
   //
   updatePtrMetaData (debugmetadataptr,
                      freeID,
-                     __builtin_return_address(1),
+                     __builtin_return_address(0),
                      (void *)SourceFilep,
                      lineno);
 
@@ -594,7 +594,7 @@ _internal_poolunregister (DebugPoolTy *Pool,
     if (debugmetadataptr->allocationType != Heap) {
         DebugViolationInfo v;
         v.type = ViolationInfo::FAULT_NOTHEAP_FREE,
-        v.faultPC = __builtin_return_address(1);
+        v.faultPC = __builtin_return_address(0);
         v.PoolHandle = Pool;
         v.dbgMetaData = debugmetadataptr;
         v.SourceFile = SourceFilep;
@@ -611,7 +611,7 @@ _internal_poolunregister (DebugPoolTy *Pool,
   if (allocaptr != start) {
     OutOfBoundsViolation v;
     v.type = ViolationInfo::FAULT_INVALID_FREE,
-      v.faultPC = __builtin_return_address(1),
+      v.faultPC = __builtin_return_address(0),
       v.faultPtr = allocaptr,
       v.dbgMetaData = debugmetadataptr,
       v.SourceFile = SourceFilep,
