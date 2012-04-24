ee46770145c424f6ec5d00b1dbf41b7af31a75a0
criswell <criswell@91177308-0d34-0410-b5e6-96231b3b80d8>
2012-04-24 16:26:53 +0000

    Added more buffer overflow tests. Modified Makefile so that the core tests are removed by the litclean target. Marked the lit-clean target as a phony target.
    git-svn-id: http://llvm.org/svn/llvm-project/safecode/trunk@155447 91177308-0d34-0410-b5e6-96231b3b80d8

---
+++ test/core/buffer-006.c
@@ -0,0 +1,21 @@
+// RUN: test.sh -p -t %t %s
+//
+// TEST: buffer-002
+//
+// Description:
+//  Test that staying within the bounds of an array never causes a problem.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int
+main (int argc, char ** argv) {
+  int index;
+  int sum;
+  volatile char * array = malloc (sizeof (char) * 1024);
+  for (index = 0; index < 1024; ++index)
+    sum += array[index];
+  return sum;
+}
+
