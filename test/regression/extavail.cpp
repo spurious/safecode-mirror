d9fcdf5ed9911e8cd916d4e382e2888f2f086758
criswell <criswell@91177308-0d34-0410-b5e6-96231b3b80d8>
2013-09-06 17:21:57 +0000

    Make this test an expected failure on Linux until I can sort out how to make it reliably work on both Mac OS X and Linux.
    git-svn-id: http://llvm.org/svn/llvm-project/safecode/trunk@190174 91177308-0d34-0410-b5e6-96231b3b80d8

--- test/regression/extavail.cpp
+++ test/regression/extavail.cpp
@@ -1,6 +1,8 @@
 //
 // RUN: clang -S -emit-llvm -O2 -fmemsafety %s -o - 2>&1 | grep TargetList 2>&1 | grep internal | grep -v _ZNSaIcEC1Ev | wc -l | grep "      0"
 //
+// XFAIL: linux
+//
 // This is a test case for PR#16672:
 // http://llvm.org/bugs/show_bug.cgi?id=16672
 //
